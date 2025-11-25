d5791b60c5e75df8a3bf7df77336d0944a4d87a1
Logan Mo <itsluqman55@gmail.com>
2025-11-25 20:09:59 +0500

    GPT2 and training

---
+++ Concepts/GPT2/train_gpt2.py
@@ -0,0 +1,295 @@
+from dataclasses import dataclass
+import math
+import torch
+import tiktoken
+import torch.nn as nn
+from torch.nn import functional as F
+
+from typing import Tuple, Dict, Optional
+
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        assert config.n_embd % config.n_head == 0
+
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+
+        self.register_buffer(
+            "bias",
+            torch.tril(torch.ones(config.block_size, config.block_size)).view(
+                1, 1, config.block_size, config.block_size
+            ),
+        )
+
+    def forward(self, x):
+        B, T, C = x.size()  # (B, T, C) batch_size, sequence_length, embedding_channels
+
+        qkv = self.c_attn(x)
+
+        q, k, v = qkv.split(self.n_embd, dim=2)  # B, T,
+
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))
+        att = F.softmax(att, dim=1)
+
+        y = att @ v
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = self.c_proj(y)
+        return x
+
+
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate="tanh")
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.proj = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.proj(self.ln_2(x))
+        return x
+
+
+@dataclass
+class GPTConfig:
+    block_size: int = 1024
+    vocab_size: int = 50257
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
+    dropout: float = 0.0
+    bias: bool = True
+
+
+class GPT(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(
+            dict(
+                wte=nn.Embedding(config.vocab_size, config.n_embd),
+                wpe=nn.Embedding(config.vocab_size, config.n_embd),
+                drop=nn.Dropout(config.dropout),
+                h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+                ln_f=nn.LayerNorm(config.n_embd),
+            )
+        )
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size)
+
+        self.transformer.wte.weight = self.lm_head.weight
+
+    def forward(self, idx, targets=None) -> Tuple[torch.Tensor, torch.Tensor]:
+        device = idx.device
+        b, t = idx.size()
+        assert (
+            t <= self.config.block_size
+        ), f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
+        pos = torch.arange(0, t, dtype=torch.long, device=device)  # shape (t)
+
+        # forward the GPT model itself
+        tok_emb = self.transformer.wte(idx)  # token embeddings of shape (b, t, n_embd)
+        pos_emb = self.transformer.wpe(pos)  # position embeddings of shape (t, n_embd)
+        x = self.transformer.drop(tok_emb + pos_emb)
+        for block in self.transformer.h:
+            x = block(x)
+        x = self.transformer.ln_f(x)
+
+        if targets is not None:
+            # if we are given some desired targets also calculate the loss
+            logits = self.lm_head(x)
+            loss = F.cross_entropy(
+                logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1
+            )
+        else:
+            # inference-time mini-optimization: only forward the lm_head on the very last position
+            logits = self.lm_head(
+                x[:, [-1], :]
+            )  # note: using list [-1] to preserve the time dim
+            loss = None
+
+        return logits, loss
+
+    @classmethod
+    def from_pretrained(cls, model_type, override_args=None):
+        assert model_type in {"gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"}
+        override_args = override_args or {}  # default to empty dict
+        # only dropout can be overridden see more notes below
+        assert all(k == "dropout" for k in override_args)
+        from transformers import GPT2LMHeadModel
+
+        print("loading weights from pretrained gpt: %s" % model_type)
+
+        # n_layer, n_head and n_embd are determined from model_type
+        config_args = {
+            "gpt2": dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            "gpt2-medium": dict(n_layer=24, n_head=16, n_embd=1024),  # 350M params
+            "gpt2-large": dict(n_layer=36, n_head=20, n_embd=1280),  # 774M params
+            "gpt2-xl": dict(n_layer=48, n_head=25, n_embd=1600),  # 1558M params
+        }[model_type]
+        print("forcing vocab_size=50257, block_size=1024, bias=True")
+        config_args["vocab_size"] = 50257  # always 50257 for GPT model checkpoints
+        config_args["block_size"] = 1024  # always 1024 for GPT model checkpoints
+        config_args["bias"] = True  # always True for GPT model checkpoints
+        # we can override the dropout rate, if desired
+        if "dropout" in override_args:
+            print(f"overriding dropout rate to {override_args['dropout']}")
+            config_args["dropout"] = override_args["dropout"]
+        # create a from-scratch initialized minGPT model
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [
+            k for k in sd_keys if not k.endswith(".attn.bias")
+        ]  # discard this mask / buffer, not a param
+
+        # init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [
+            k for k in sd_keys_hf if not k.endswith(".attn.masked_bias")
+        ]  # ignore these, just a buffer
+        sd_keys_hf = [
+            k for k in sd_keys_hf if not k.endswith(".attn.bias")
+        ]  # same, just the mask (buffer)
+        transposed = [
+            "attn.c_attn.weight",
+            "attn.c_proj.weight",
+            "mlp.c_fc.weight",
+            "mlp.c_proj.weight",
+        ]
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(
+            sd_keys
+        ), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+
+
+class DataLoader:
+    def __init__(self, batch_size, context_length):
+        self.batch_size = batch_size
+        self.context_lenth = context_length
+
+        with open(TEXT_DATA_PATH, "r") as file:
+            text_data = file.read()
+
+        enc = tiktoken.get_encoding("gpt2")
+        tokens = enc.encode(text_data)
+        self.tokens = torch.tensor(tokens)
+
+        print(f"Loaded {len(self.tokens)} Tokens")
+        print(f"1 epoch =  {len(self.tokens) // (batch_size*context_length)} Batches")
+
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.batch_size, self.context_lenth
+
+        buf = self.tokens[self.current_position : self.current_position + (B * T) + 1]
+        x = buf[:-1].view(B, T)
+        y = buf[1:].view(B, T)
+
+        self.current_position += B * T
+        if self.current_position + (B * T + 1) > len(self.tokens):
+            self.current_position = 0
+
+        return x, y
+
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+TEXT_DATA_PATH = "tiny_shiekspear.txt"
+LEARNING_RATE = 3e-4
+EPOCHS = 2640
+BATCH_SIZE = 4
+CONTEXT_WINDOW = 32
+
+model = GPT(GPTConfig())
+model.to(DEVICE)
+
+train_loader = DataLoader(BATCH_SIZE, CONTEXT_WINDOW)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
+for i in range(EPOCHS):
+    optimizer.zero_grad()
+    x, y = train_loader.next_batch()
+    logits, loss = model(x.to(DEVICE), y.to(DEVICE))
+    loss.backward()
+    optimizer.step()
+    print(f"step {i}, loss: {loss.item()}")
+
+
+# Eval
+num_return_sequences = 5
+max_length = 30
+model.eval()
+
+enc = tiktoken.get_encoding("gpt2")
+tokens = enc.encode("Hello! I'm a language model,")
+tokens = torch.tensor(tokens, dtype=torch.long)
+tokens = torch.unsqueeze(tokens, 0).repeat(num_return_sequences, 1)
+x = tokens.to(DEVICE)
+
+while x.size(1) < max_length:
+    with torch.no_grad():
+        logits, loss = model(x)
+        logits = logits[:, -1, :]
+        probs = F.softmax(logits, dim=-1)
+        topk_probs, topk_inidces = torch.topk(probs, 50, dim=-1)
+        ix = torch.multinomial(topk_probs, 1)
+        xcol = torch.gather(topk_inidces, -1, ix)
+        x = torch.cat((x, xcol), dim=1)
+
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
---
+++ Concepts/Transformer-from-Scratch/transformer.py
@@ -0,0 +1,260 @@
+import time
+
+import torch
+import numpy as np
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
+
+# Parameters
+TEXT_DATA_PATH = "tiny_shiekspear.txt"
+TRAIN_SPLIT = 0.9
+VOCAB_SIZE = 65
+BLOCK_SIZE = 256
+
+N_EMBED = HEAD_SIZE = 384
+N_HEADS = 6
+N_LAYER = 6
+
+DROPOUT = 0.2
+BATCH_SIZE = 64
+LEARNING_RATE = 3e-4
+EVAL_INTERVAL = 500
+EVAL_ITERS = 200
+EPOCHS = 5000
+
+MAX_NEW_TOKENS = 1000
+
+DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+# DEVICE = torch.device("cpu")
+
+with open(TEXT_DATA_PATH, "r") as file:
+    text_data = file.read()
+
+chars = sorted(list(set(text_data)))
+stoi = {ch: i for i, ch in enumerate(chars)}
+itos = {i: ch for i, ch in enumerate(chars)}
+
+
+def encode(text):
+    return [stoi[c] for c in text]
+
+
+def decode(indices):
+    return "".join([itos[i] for i in indices])
+
+
+def get_batch(data, batch_size, block_size):
+    indices = torch.randint(0, len(data) - block_size - 1, (batch_size,))
+    X = torch.stack([data[idx : idx + block_size] for idx in indices])
+    y = torch.stack([data[idx + 1 : idx + block_size + 1] for idx in indices])
+    return X, y
+
+
+@torch.no_grad()
+def estimate_loss(model: nn.Module, eval_iters, train_data, val_data):
+    out = {}
+    model.eval()
+
+    for split in ["train", "val"]:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, y = (
+                get_batch(train_data, BATCH_SIZE, BLOCK_SIZE)
+                if split == "train"
+                else get_batch(val_data, BATCH_SIZE, BLOCK_SIZE)
+            )
+            logits, loss = model(X, y)
+
+            losses[k] = loss
+
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+
+train_data_tensor = torch.tensor(encode(text_data), dtype=torch.long).to(DEVICE)
+train_data = train_data_tensor[: int(len(train_data_tensor) * TRAIN_SPLIT)]
+val_data = train_data_tensor[int(len(train_data_tensor) * TRAIN_SPLIT) :]
+
+
+class Head(nn.Module):
+    def __init__(self, n_embed, head_size, block_size, dropout):
+        super().__init__()
+
+        self.n_embed = n_embed
+
+        self.queries = nn.Linear(n_embed, head_size, bias=False)
+        self.keys = nn.Linear(n_embed, head_size, bias=False)
+        self.values = nn.Linear(n_embed, head_size, bias=False)
+
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+
+        B, T, C = x.shape
+
+        q = self.queries(x)
+        k = self.keys(x)
+        v = self.values(x)
+
+        wei = q @ k.transpose(-2, -1) * C**-0.5
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
+        wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
+
+        x = wei @ v
+
+        return x
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, n_heads, n_embed, head_size, block_size, dropout):
+        super().__init__()
+        self.sa_heads = nn.ModuleList(
+            [
+                Head(n_embed, head_size // n_heads, block_size, dropout)
+                for _ in range(n_heads)
+            ]
+        )
+        self.proj = nn.Linear(
+            n_embed, n_embed
+        )  # Projecting back into the residual pathway
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x = torch.concat([head(x) for head in self.sa_heads], dim=-1)
+        x = self.proj(x)
+        x = self.dropout(x)
+        return x
+
+
+class Forward(nn.Module):
+    def __init__(self, head_size, dropout):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(
+                head_size, 4 * head_size
+            ),  # Multiplying by 4 because the transformer paper does say. They have 512 inputs and 2048 hidden dims, so we multiply by 4
+            nn.ReLU(),
+            nn.Linear(
+                4 * head_size, head_size
+            ),  # Projecting back into the residual pathway
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Block(nn.Module):
+    def __init__(self, n_heads, n_embed, head_size, block_size, dropout):
+        super().__init__()
+        self.sa_heads = MultiHeadAttention(
+            n_heads, n_embed, head_size, block_size, dropout
+        )  # Acts as a communication layer between tokens
+        self.ffwd = Forward(head_size, dropout)  # Acts as computation within a node
+        self.ln1 = nn.LayerNorm(n_embed)
+        self.ln2 = nn.LayerNorm(n_embed)
+
+    def forward(self, x):
+        x = x + self.sa_heads(
+            self.ln1(x)
+        )  # Adding residual connections becuase the network is too deep and learning is bad
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
+
+class BigramLanguageModel(nn.Module):
+    def __init__(
+        self, vocab_size, block_size, n_embed, head_size, n_heads, n_blocks, dropout
+    ) -> None:
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.block_size = block_size
+        self.n_embed = n_embed
+
+        self.token_embeddings = nn.Embedding(vocab_size, n_embed)
+        self.positional_embeddings = nn.Embedding(block_size, n_embed)
+
+        self.blocks = nn.Sequential(
+            *[
+                Block(n_heads, n_embed, head_size, block_size, dropout)
+                for _ in range(n_blocks)
+            ]
+        )
+
+        self.ln_f = nn.LayerNorm(n_embed)
+
+        self.lm_head = nn.Linear(n_embed, vocab_size)
+
+    def forward(self, x, targets=None):
+        B, T = x.shape
+        token_emb = self.token_embeddings(x)
+        position_emb = self.positional_embeddings(torch.arange(0, T, device=x.device))
+
+        x = token_emb + position_emb
+        x = self.blocks(x)
+        x = self.ln_f(x)
+        logits = self.lm_head(x)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C)
+            targets = targets.view(B * T)
+            loss = F.cross_entropy(logits, targets)
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+
+            idx_cond = idx[:, -self.block_size :]
+
+            logits, _ = self(idx_cond)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
+
+
+writer = SummaryWriter()
+model = BigramLanguageModel(
+    VOCAB_SIZE, BLOCK_SIZE, N_EMBED, HEAD_SIZE, N_HEADS, N_LAYER, DROPOUT
+).to(DEVICE)
+optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
+
+start_time = time.time()
+for steps in range(EPOCHS):
+
+    if steps + 1 % EVAL_INTERVAL == 0:
+        losses = estimate_loss(model, EVAL_ITERS, train_data, val_data)
+        print(
+            f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}"
+        )
+        writer.add_scalar("Loss/train", losses["train"], steps + 1)
+        writer.add_scalar("Loss/val", losses["val"], steps + 1)
+
+    xb, yb = get_batch(train_data, BATCH_SIZE, BLOCK_SIZE)
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+print(f"Total Time Taken: {time.time() - start_time}")
+
+print(loss.item())
+writer.flush()
+writer.close()
+
+print(
+    decode(
+        model.generate(
+            torch.ones(1, 1, dtype=torch.long, device=DEVICE), MAX_NEW_TOKENS
+        )[0].tolist()
+    )
+)
